c25aafba6c12a71b103b4955afa9430a89d47923
Keygoman <39460584+keygoman@users.noreply.github.com>
2021-01-08 17:57:11 +0800

    Update download_Jdimage.py

--- download_Jdimage.py
+++ download_Jdimage.py
@@ -29,42 +29,42 @@ async def get_img_address(skuId):
                 html = await resp.read()
                 ul= etree.HTML(html).xpath('//*[@id="spec-list"]/ul/li/img')
                 for li in ul:
-                    img_address = str(li.get('src'))
-                    img_address.replace('n5','imgzone',1)
-                    img_address.replace('s50x64_','',1)
+                    temp_address = str(li.get('src')).replace('n5','imgzone',1)
+                    # img_address.replace('n5','imgzone',1)
+                    img_address = temp_address.replace('s50x64_','',1)
                     img_list.append(img_address)
-    return img_list
+                if len(img_list) != 0:
+                    count = 1
+                    for img in img_list:
+                        async with session1.get(url='https:'+img,headers=header) as image:
+                            imgcontent = await image.read()
+                            with open('./'+str(skuId)+'/'+str(count)+'.jpg','wb') as f:
+                                f.write(imgcontent)
+                        count = count + 1        
 
-# 下载单张图片方法
-async def download_img(img_address,count,product_id):
-    async with sem:
-        async with aiohttp.ClientSession() as session2:
-            async with session2.request('GET',url=img_address,headers=header) as img_resp:
-                with open('./'+str(product_id)+'/'+str(count)+'.jpg','wb') as f:
-                    f.write(img_resp.content)
-
-def run():
-    product_ids = get_prodcut_skuId()
-    if len(product_ids) != 0:
-        for id in product_ids:
-            os.mkdir('./'+str(id)+'/')
-            img_address_list =  get_img_address(id)
-            count = 1
-            for img_address in img_address_list:
-                download_img(img_address,count,id)
-                count = count + 1
-    else:
-        print('都没东西，你玩你m呢')
+# # 下载单张图片方法
+# async def download_img(img_list,product_id):
+#     count = 1
+#     for img_address in img_list:
+#         async with sem:
+#             async with aiohttp.ClientSession() as session2:
+#                 async with session2.request('GET',url=img_address,headers=header) as img_resp:
+#                     with open('./'+str(product_id)+'/'+str(count)+'.jpg','wb') as f:
+#                         f.write(img_resp.content)
+#         count = count + 1
 
 def main():
     product_ids = get_prodcut_skuId()
     if len(product_ids) != 0:
         for id in product_ids:
-            os.mkdir('./'+str(id)+'/')
-
+            try:
+                os.mkdir('./'+str(id)+'/')
+            except FileExistsError as identifier:
+                pass    
+    else:
+        print('都没东西，你玩你m呢')
     loop = asyncio.get_event_loop()
     tasks = [get_img_address(id) for id in product_ids]
-
     loop.run_until_complete(asyncio.wait(tasks))
     loop.close()
 
